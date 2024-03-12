dfc1e6c73188c3084165478f5a8153d78f94825b
vicgrabru <vgravanbru@gmail.com>
2024-03-12 11:16:10 +0100

    Added ManagerDashboard functionality

---
+++ src/main/java/acme/features/manager/managerDashboard/ManagerManagerDashboardShowService.java
@@ -0,0 +1,119 @@
+/*
+ * AdministratorDashboardShowService.java
+ *
+ * Copyright (C) 2012-2024 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.manager.managerDashboard;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.datatypes.Money;
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.configuration.SystemConfiguration;
+import acme.forms.ManagerDashboard;
+import acme.roles.Manager;
+import acme.utils.MoneyUtils;
+
+@Service
+public class ManagerManagerDashboardShowService extends AbstractService<Manager, ManagerDashboard> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private ManagerManagerDashboardRepository repository;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		ManagerDashboard dashboard;
+
+		Integer totalNumberOfUserStoriesWithMustPriority;
+		Integer totalNumberOfUserStoriesWithShouldPriority;
+		Integer totalNumberOfUserStoriesWithCouldPriority;
+		Integer totalNumberOfUserStoriesWithWontPriority;
+
+		Double avgEstimatedCostOfUserStories;
+		Integer minEstimatedCostOfUserStories;
+		Integer maxEstimatedCostOfUserStories;
+		Double stdEstimatedCostOfUserStories;
+
+		Money avgCostOfProjects;
+		Money minCostOfProjects;
+		Money maxCostOfProjects;
+		Money stdCostOfProjects;
+
+		Collection<Money> costOfProjects;
+		SystemConfiguration systemConfiguration;
+
+		totalNumberOfUserStoriesWithMustPriority = this.repository.totalNumberOfUserStoriesWithMustPriority().intValue();
+		totalNumberOfUserStoriesWithShouldPriority = this.repository.totalNumberOfUserStoriesWithShouldPriority().intValue();
+		totalNumberOfUserStoriesWithCouldPriority = this.repository.totalNumberOfUserStoriesWithCouldPriority().intValue();
+		totalNumberOfUserStoriesWithWontPriority = this.repository.totalNumberOfUserStoriesWithWontPriority().intValue();
+
+		avgEstimatedCostOfUserStories = this.repository.avgEstimatedCostOfUserStories();
+		minEstimatedCostOfUserStories = this.repository.minEstimatedCostOfUserStories();
+		maxEstimatedCostOfUserStories = this.repository.maxEstimatedCostOfUserStories();
+		stdEstimatedCostOfUserStories = this.repository.stdEstimatedCostOfUserStories();
+
+		costOfProjects = this.repository.getCostOfAllProjects();
+		systemConfiguration = this.repository.getSystemConfiguration();
+
+		avgCostOfProjects = MoneyUtils.getAvg(costOfProjects, systemConfiguration.getSystemCurrency());
+		minCostOfProjects = MoneyUtils.getMin(costOfProjects, systemConfiguration.getSystemCurrency());
+		maxCostOfProjects = MoneyUtils.getMax(costOfProjects, systemConfiguration.getSystemCurrency());
+		stdCostOfProjects = MoneyUtils.getStd(costOfProjects, systemConfiguration.getSystemCurrency());
+
+		dashboard = new ManagerDashboard();
+
+		dashboard.setTotalNumberOfUserStoriesWithMustPriority(totalNumberOfUserStoriesWithMustPriority);
+		dashboard.setTotalNumberOfUserStoriesWithShouldPriority(totalNumberOfUserStoriesWithShouldPriority);
+		dashboard.setTotalNumberOfUserStoriesWithCouldPriority(totalNumberOfUserStoriesWithCouldPriority);
+		dashboard.setTotalNumberOfUserStoriesWithWontPriority(totalNumberOfUserStoriesWithWontPriority);
+
+		dashboard.setAvgEstimatedCostOfUserStories(avgEstimatedCostOfUserStories);
+		dashboard.setMinEstimatedCostOfUserStories(minEstimatedCostOfUserStories);
+		dashboard.setMaxEstimatedCostOfUserStories(maxEstimatedCostOfUserStories);
+		dashboard.setStdEstimatedCostOfUserStories(stdEstimatedCostOfUserStories);
+
+		dashboard.setAvgCostOfProjects(avgCostOfProjects);
+		dashboard.setMinCostOfProjects(minCostOfProjects);
+		dashboard.setMaxCostOfProjects(maxCostOfProjects);
+		dashboard.setStdCostOfProjects(stdCostOfProjects);
+
+		super.getBuffer().addData(dashboard);
+	}
+
+	@Override
+	public void unbind(final ManagerDashboard object) {
+		Dataset dataset;
+
+		dataset = super.unbind(object, //
+			"totalNumberOfUserStoriesWithMustPriority", "totalNumberOfUserStoriesWithShouldPriority", //
+			"totalNumberOfUserStoriesWithCouldPriority", "totalNumberOfUserStoriesWithWontPriority", //
+			"avgEstimatedCostOfUserStories", "minEstimatedCostOfUserStories", //
+			"maxEstimatedCostOfUserStories", "stdEstimatedCostOfUserStories", //
+			"avgCostOfProjects", "minCostOfProjects", //
+			"maxCostOfProjects", "stdCostOfProjects");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
